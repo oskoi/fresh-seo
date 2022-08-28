cdc18b2f03eb6e267d9597f277b2c327d800f1eb
oleg.skovpen <oskovpen@megogo.net>
2022-08-28 18:23:06 +0300

    Add defaultWeight; Support dir routes

--- src/sitemap.ts
+++ src/sitemap.ts
@@ -7,11 +7,13 @@ import day from "dayjs";
 import { basename, extname } from "path";
 import { Manifest } from "fresh/server.ts";
 
+const defaultWeight = 0.8;
+
 export class SitemapContext {
   #url: string;
   #manifest: Manifest;
   #globalIgnore = ["sitemap.xml"];
-  #additionalRoutes: Array<string> = [];
+  #additionalRoutes: Map<string, number> = new Map();
 
   constructor(url: string, manifest: Manifest) {
     this.#url = url;
@@ -36,32 +38,43 @@ export class SitemapContext {
 
           return true;
         })
-        .map(([path, route]) => {
-          return path
+        .map(([path, _]) => {
+          const npath = path
             .replace(extname(path), "")
             .replace("./routes", "")
             .replace("index", ""); // We remove index as it's consider a "/" in Fresh
+
+          return [npath, defaultWeight];
         }),
 
       ...this.#additionalRoutes,
     ];
   }
 
-  add(route: string) {
-    this.#additionalRoutes.push(route);
+  async add(route: string, weight = defaultWeight) {
+    try {
+      for await (const item of Deno.readDir(route)) {
+        if (item.isFile) {
+          this.#additionalRoutes.set(item.name, weight);
+        }
+      }
+    } catch (_e: any) {
+      this.#additionalRoutes.set(route, weight);
+    }
+
     return this;
   }
 
   generate() {
     return `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
       ${
-      this.routes.map((route) => {
+      this.routes.map(([route, weight]) => {
         return `<url>
           <loc>${this.#url}${route}</loc>
           <lastmod>${day().format("YYYY-MM-DD")}</lastmod>
           <changefreq>daily</changefreq>
-          <priority>0.8</priority>
+          <priority>${weight}</priority>
         </url>`;
       })
         .join("\n")
@@ -74,19 +87,4 @@ export class SitemapContext {
       headers: { "Content-Type": "application/xml" },
     });
   }
-
-  // async save() {
-  //   const outPath = join(this.#staticDir, "sitemap.xml");
-
-  //   try {
-  //     const content = this.generate();
-
-  //     await ensureFile(outPath);
-
-  //     return Deno.writeTextFile(outPath, content);
-  //   } catch (e) {
-  //     console.warn(e.message);
-  //     return null;
-  //   }
-  // }
 }
